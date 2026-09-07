e3f140eeacc22d46a03cbbb7eb8d7ff72466dbed
nischaykishore1904-spec <nischaykishore1904@gmail.com>
2026-09-08 01:13:04 +0530

    Time: 0 ms (100.00%) | Memory: 27.2 MB (52.23%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int st = 0, end = s.size() - 1;
-        while(st < end){
-            swap(s[st++], s[end--]);
-            
-        }
+        reverse(s.begin(), s.end());
     }
 };

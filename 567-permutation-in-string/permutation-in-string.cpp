93d4ce90427041e85545cf26c5972be444028407
nischaykishore1904-spec <nischaykishore1904@gmail.com>
2026-09-09 02:29:37 +0530

    Time: 84 ms (23.42%) | Memory: 9.9 MB (89.49%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -0,0 +1,46 @@
+class Solution {
+    bool isFreqSame(int freq1[], int freq2[]) {
+        for(int i = 0; i < 26; i++) {
+            if(freq1[i] != freq2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+public:
+    bool checkInclusion(string s1, string s2) {
+
+        if(s1.length() > s2.length()) {
+            return false;
+        }
+
+        int freq[26] = {0};
+
+        for(int i = 0; i < s1.length(); i++) {
+            freq[s1[i] - 'a']++;
+        }
+
+        int windSize = s1.length();
+
+        for(int i = 0; i <= s2.length() - windSize; i++) {
+
+            int windFreq[26] = {0};
+
+            int idx = i;
+            int windIdx = 0;
+
+            while(windIdx < windSize) {
+                windFreq[s2[idx] - 'a']++;
+                windIdx++;
+                idx++;
+            }
+
+            if(isFreqSame(freq, windFreq)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};

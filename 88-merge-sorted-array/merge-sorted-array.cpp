35aac9371da68f7ea2e5181387c91bf12e602f5a
nischaykishore1904-spec <nischaykishore1904@gmail.com>
2026-09-10 17:44:04 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (55.33%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = 0, j = 0;
-        vector<int> ans;
-        while(i < m && j < n){
+        int i = m - 1;//last element of nums1
+        int j = n - 1;//last element of nums2
+        int k = m + n - 1;//last index of nums1
+        
+        while(i >= 0 && j >= 0){
             
-            if(nums1[i] <= nums2[j]){
-                ans.push_back(nums1[i]);
-                i++;
+            if(nums1[i] > nums2[j]){
+                nums1[k] = nums1[i];
+                i--;
             }
             else{
-                ans.push_back(nums2[j]);
-                j++;
+                nums1[k] = nums2[j];
+                j--;
             }
-        }
-        while(i < m){
-            ans.push_back(nums1[i]);
-            i++;
+            k--;
 
         }
-        while(j < n){
-            ans.push_back(nums2[j]);
-            j++;
-        }
-        for(i = 0; i < m+n; i++){
-            nums1[i] = ans[i];
+        //if nums2 still have elements left
+        while(j >= 0){
+            nums1[k] = nums2[j];
+            j--; k--;
         }
 
         for(int val : nums1){
